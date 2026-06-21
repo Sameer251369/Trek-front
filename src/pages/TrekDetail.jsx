40ef1384e6dc9a428b25448ee6aa8ffb76ede1be
Sameer <sameerbashir522@gmail.com>
2026-06-22 04:56:48 +0530

    cxx

--- src/pages/TrekDetail.jsx
+++ src/pages/TrekDetail.jsx
@@ -1,8 +1,7 @@
 
-import React, { useState,useEffect } from 'react';
+import React, { useState, } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { Send } from 'lucide-react';
 import {
   MessageSquare,
   Map,
